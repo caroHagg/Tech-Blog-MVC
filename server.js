317920a4f84b00e88b92a2327f9c9d315c6375f2
Carolina Haggerty <work@Carolinas-Mac.local>
2023-05-07 17:56:38 -0700

    added MVC structure folder and some boilerplate code to server.js. Added columns to models and some relations between tables

--- server.js
+++ server.js
@@ -1,22 +1,33 @@
-const path = require('path');
-const express = require('express');
 const exphbs = require('express-handlebars');
-const routes = require('./controllers');
+
 const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {
+    maxAge:1000*60*60*2
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
 
-const hbs = exphbs.create({});
-
-
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+app.use(session(sess));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+
+const hbs = exphbs.create({});
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.static("public"))
 
 app.use(routes);
 
