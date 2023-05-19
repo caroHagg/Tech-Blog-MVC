e9b81ac430949c2c2c0a54e0d023ccedb3126d68
Carolina Haggerty <carolina.vhaggerty@gmail.com>
2023-05-18 18:38:41 -0700

    added delete function for post

--- public/js/delete-update-post.js
+++ public/js/delete-update-post.js
@@ -1,12 +1,41 @@
 const updateBtn = document.querySelector("#update-btn");
 const deleteBtn = document.querySelector("#delete-btn");
 
+
 updateBtn.addEventListener("click",e=>{
     e.preventDefault();
     const updatePost = {
         title:document.querySelector("#tittle-post").value,
         content:document.querySelector("#content-post").value,
-        id:updateBtn.getAttribute("data-post-id")
     }
-    console.log(updatePost)
+    const id = updateBtn.getAttribute("data-post-id")    
+    fetch(`/api/posts/${id}`,{
+        method:"PUT",
+        body:JSON.stringify(updatePost),
+        headers:{
+            "Content-Type":"application/json"
+        }
+    }).then(res=>{
+        if(res.ok){
+            location.replace("/dashboard")
+        }else{
+            alert("No updates have been made")
+        }
+    })
+
+});
+
+deleteBtn.addEventListener("click", e=>{
+    e.preventDefault()
+    const deleteId = deleteBtn.getAttribute("data-post-id");
+    fetch(`/api/posts/${deleteId}`,{
+        method:"DELETE",
+    }).then(res=>{
+        if(res.ok){
+            location.replace("/dashboard")
+        }else{
+            alert("No post has been deleted")
+        }
+    })
+
 })
